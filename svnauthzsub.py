cb3ba04afe10f9e64d5c8c2219f6e5e55037f359
Omid Manikhi <omidontop@gmail.com>
2022-10-19 09:48:19 +0200

    A draft of the svnauthzsub with a basic structure

---
+++ svnauthzsub.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python3
+# encoding: UTF-8
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+#  svnauthzsub - Subscribe to a SvnPubSub stream, monitor the access.accs changes and regenerate the apache config file.
+#
+# Example:
+#   svnauthzsub.py
+#
+# On startup  svnauthzsub starts listening to commits in all repositories.
+#
+
+import os
+import stat
+import logging
+import argparse
+import svnpubsub.logger
+from io import StringIO
+from textwrap import indent
+from configparser import ConfigParser
+from svnpubsub.client import Commit
+from svnpubsub.daemon import Daemon, DaemonTask
+from svnpubsub.bgworker import BackgroundJob
+from svnpubsub.util import execute
+
+PORT = 2069
+HOST = "127.0.0.1"
+REPO_EXCLUDES = []
+SVNBIN_DIR = "/usr/bin"
+SVNROOT_DIR = "/srv/cms/svn"
+OUTPUT_DIR = None
+INDENTATION = 2
+
+
+def generate(access_accs, repo):
+
+    def location(path, content):
+        result = "<Location /svn/{}{} >{}".format(repo, path, os.linesep)
+        result += indent(content, "".ljust(INDENTATION))
+        result += "</Location>" + os.linesep + os.linesep
+        return result
+
+    def require_all(content):
+        result = "<RequireAll>" + os.linesep
+        result += indent(content, "".ljust(INDENTATION))
+        result += "</RequireAll>" + os.linesep
+        return result
+
+    output = StringIO()
+    config_parser = ConfigParser()
+    config_parser.read_string(access_accs)
+    # Ignore sections not containing a path such as 'groups', etc.
+    paths = [section for section in config_parser.sections() if '/' in section]
+    # Sort the paths putting parent before children as that is how it works as expected in apache
+    paths.sort(key=lambda x: (os.path.dirname(x), os.path.basename(x)))
+    for path in paths:
+        output.write(location(path, require_all("Require valid-user" + os.linesep)))
+    output.seek(0)
+    return output
+
+
+class Job(BackgroundJob):
+
+    def __init__(self, commit: Commit):
+        super().__init__(repo=commit.repositoryname, rev=commit.id, head=commit.id, commit=commit)
+
+    def retrieve_access_accs(self, rev=0):
+        global SVNBIN_DIR, SVNROOT_DIR
+        svnlook = os.path.join(SVNBIN_DIR, 'svnlook')
+        repository = os.path.join(SVNROOT_DIR, self.repo)
+        arguments = [svnlook, 'cat', repository, '/access.accs']
+        _, stdout, _ = execute(*arguments)
+        return stdout
+
+    def validate(self) -> bool:
+        return True
+
+    def run(self):
+        access_accs = self.retrieve_access_accs()
+        output_file = os.path.join(OUTPUT_DIR, "svn-{}.conf".format(self.repo))
+
+
+class Task(DaemonTask):
+
+    def __init__(self):
+        super().__init__(urls=["http://%s:%d/commits" % (HOST, PORT)])
+
+    def start(self):
+        logging.info('Daemon started.')
+
+    def commit(self, url: str, commit: Commit):
+        if commit.type != 'svn' or commit.format != 1:
+            logging.info("SKIP unknown commit format (%s.%d)", commit.type, commit.format)
+            return
+        logging.info("COMMIT r%d (%d paths) from %s" % (commit.id, len(commit.changed), url))
+
+        excluded = False
+        for repo in REPO_EXCLUDES:
+            if commit.repositoryname.startswith(repo):
+                logging.info('Commit in excluded repository, ignoring: %s' % commit.repositoryname)
+                excluded = True
+
+        if "access.accs" in commit.changed and not excluded:
+            job = Job(commit)
+            self.worker.queue(job)
+
+
+def main():
+    global SVNROOT_DIR, SVNBIN_DIR
+
+    parser = argparse.ArgumentParser(description='An SvnPubSub client that monitors the access.accs and regenerates the apache config file.')
+
+    parser.add_argument('--logfile', help='a filename for logging if stdout is not the desired output')
+    parser.add_argument('--pidfile', help='the PID file where the process PID will be written to')
+    parser.add_argument('--uid', help='switch to this UID before running')
+    parser.add_argument('--gid', help='switch to this GID before running')
+    parser.add_argument('--daemon', action='store_true', help='run as a background daemon')
+    parser.add_argument('--umask', help='set this (octal) UMASK before running')
+    parser.add_argument('--repo', help='the repository name to use in combination with --input file as input')
+    parser.add_argument('--input', help='a local access.acss file to use instead of retrieving it from a repo')
+    parser.add_argument('--output-dir', help='the path to place the generated apache configuration files')
+    parser.add_argument('--svnroot', default=SVNROOT_DIR, help='the path to repositories (default: %s)' % SVNROOT_DIR)
+    parser.add_argument('--svnbin', default=SVNBIN_DIR,
+                        help='the path to svn, svnlook, svnadmin, ... binaries (default: %s)' % SVNBIN_DIR)
+    parser.add_argument('--log-level', type=int, default=logging.INFO,
+                        help='log level (DEBUG: %d | INFO: %d | WARNING: %d | ERROR: %d | CRITICAL: %d) (default: %d)' %
+                             (logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL, logging.INFO))
+
+    args = parser.parse_args()
+
+    if args.input and not args.repo:
+        parser.error('REPO is required when INPUT is supplied')
+
+    if args.input:
+        if not os.path.exists(args.input):
+            parser.error('Filed to find the input file: {}'.format(args.input))
+        with open(args.input, 'r') as file:
+            access_accs = file.read()
+            config = generate(access_accs=access_accs, repo=args.repo)
+            print(config.read())
+        exit(0)
+    elif not args.output_dir:
+        parser.error('OUTPUT_DIR must be provided')
+
+    if args.svnbin and args.svnroot:
+        SVNBIN_DIR = args.svnbin
+        SVNROOT_DIR = args.svnroot
+    else:
+        parser.error('SVNBIN and SVNROOT must be provided')
+
+    # In daemon mode, we let the daemonize module handle the pidfile.
+    # Otherwise, we should write this (foreground) PID into the file.
+    if args.pidfile and not args.daemon:
+        pid = os.getpid()
+        # Be wary of symlink attacks
+        try:
+            os.remove(args.pidfile)
+        except OSError:
+            pass
+        flags = os.O_WRONLY | os.O_CREAT | os.O_EXCL, stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH
+        with os.open(args.pidfile, flags) as f:
+            os.write(f, b'%d\n' % pid)
+            logging.info('PID: %d -> %s', pid, args.pidfile)
+
+    if args.gid:
+        try:
+            gid = int(args.gid)
+        except ValueError:
+            import grp
+            gid = grp.getgrnam(args.gid)[2]
+        logging.info('GID: %d', gid)
+        os.setgid(gid)
+
+    if args.uid:
+        try:
+            uid = int(args.uid)
+        except ValueError:
+            import pwd
+            uid = pwd.getpwnam(args.uid)[2]
+        logging.info('UID: %d', uid)
+        os.setuid(uid)
+
+    # Setup a new logging handler with the specified log level
+    svnpubsub.logger.setup(logfile=args.logfile, level=args.log_level)
+
+    if args.daemon and not args.logfile:
+        parser.error('LOGFILE is required when running as a daemon')
+    if args.daemon and not args.pidfile:
+        parser.error('PIDFILE is required when running as a daemon')
+
+    # We manage the logfile ourselves (along with possible rotation).
+    # The daemon process can just drop stdout/stderr into /dev/null.
+    daemon = Daemon(name=os.path.basename(__file__),
+                    logfile='/dev/null',
+                    pidfile=os.path.abspath(args.pidfile) if args.pidfile else None,
+                    umask=args.umask,
+                    task=Task())
+    if args.daemon:
+        # Daemonize the process and call sys.exit() with appropriate code
+        daemon.daemonize_exit()
+    else:
+        # Just run in the foreground (the default)
+        daemon.foreground()
+
+
+if __name__ == "__main__":
+    main()
