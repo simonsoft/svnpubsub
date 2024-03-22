4c1cfe7e4f35c235558902e176911da7443be0ac
Omid Manikhi <omidontop@gmail.com>
2024-03-22 11:24:47 +0100

    Improved the execute() function fixing an issue that prevented it from reading the entire stdout/stderr.

--- svnpubsub/util.py
+++ svnpubsub/util.py
@@ -47,12 +47,14 @@ def is_file_like(variable) -> bool:
 
 
 def execute(*args, text=True, env=None, throw=True, stdin=None, stdout=None, stderr=None):
+    rlist = []
+    wlist = []
+    xlist = []
     process = None
     arguments = [*args]
-    rlist = wlist = xlist = []
     chunk_size = 1 * 1024 * 1024    # 1 MB
-    stdout_buffer = None if stdout is not None else [] if text else bytes()
-    stderr_buffer = None if stderr is not None else [] if text else bytes()
+    stdout_buffer = None if stdout is not None else [] if text else bytearray()
+    stderr_buffer = None if stderr is not None else [] if text else bytearray()
     stdin_writer = None
 
     if stdin is not None and not is_file_like(stdin):
@@ -64,7 +66,7 @@ def execute(*args, text=True, env=None, throw=True, stdin=None, stdout=None, std
 
     logging.debug("Running: %s", " ".join(arguments))
 
-    def writer(source, destination):
+    def write(source, destination):
         """
         Copy the source file-like object to the destination file-like object and close
         the destination file-like object.
@@ -75,42 +77,69 @@ def execute(*args, text=True, env=None, throw=True, stdin=None, stdout=None, std
         copyfileobj(source, destination, chunk_size)
         destination.close()
 
+    def read(p, f, o, e) -> (bool, bool):
+        """
+        Reads a chunk or a line of the available data and add them to the stdout and stderr buffers.
+        @param p: The process from which to read the stdout or stderr data
+        @param f: The file descriptor of the corresponding stdout or stderr object
+        @param o: The stdout buffer to write to
+        @param e: The stderr buffer to write to
+        @return: A boolean tuple indicating whether there's more to read for either stdout or stderr
+        """
+        more_o = False
+        more_e = False
+        if f == p.stdout.fileno():
+            if stdout is None:
+                if text:
+                    line = p.stdout.readline()
+                    if line:
+                        o.append(line.rstrip())
+                        more_o = True
+                else:
+                    chunk = p.stdout.read(chunk_size)
+                    if chunk:
+                        o += chunk
+                        more_o = True
+            else:
+                chunk = p.stdout.read(chunk_size)
+                if chunk:
+                    stdout.write(chunk)
+                    more_o = True
+        if f == p.stderr.fileno():
+            if stderr is None:
+                if text:
+                    line = p.stderr.readline()
+                    if line:
+                        e.append(line.rstrip())
+                        more_e = True
+                else:
+                    chunk = p.stderr.read(chunk_size)
+                    if chunk:
+                        e += chunk
+                        more_e = True
+            else:
+                chunk = p.stderr.read(chunk_size)
+                if chunk:
+                    stderr.write(chunk)
+                    more_e = True
+
+        return more_o, more_e
+
     try:
         process = Popen(arguments, text=text, universal_newlines=text, env=env,
                         stdin=PIPE if stdin is not None else None, stdout=PIPE, stderr=PIPE)
+
         if stdin is not None and process.poll() is None:
             # If stdin was supplied, copy its contents to the stdin stream of the process in a separate thread
-            stdin_writer = Thread(target=writer, args=[stdin, process.stdin])
+            stdin_writer = Thread(target=write, args=[stdin, process.stdin])
             stdin_writer.start()
         while process.poll() is None:
             rlist += [process.stdout.fileno(), process.stderr.fileno()]
             for fd in [item for sublist in select(rlist, wlist, xlist) for item in sublist]:
-                if fd == process.stdout.fileno():
-                    if stdout is None:
-                        if text:
-                            line = process.stdout.readline()
-                            stdout_buffer.append(line.rstrip())
-                        else:
-                            chunk = process.stdout.read(chunk_size)
-                            if chunk:
-                                stdout_buffer += chunk
-                    else:
-                        chunk = process.stdout.read(chunk_size)
-                        if chunk:
-                            stdout.write(chunk)
-                if fd == process.stderr.fileno():
-                    if stderr is None:
-                        if text:
-                            line = process.stderr.readline()
-                            stderr_buffer.append(line.rstrip())
-                        else:
-                            chunk = process.stderr.read(chunk_size)
-                            if chunk:
-                                stderr_buffer += chunk
-                    else:
-                        chunk = process.stderr.read(chunk_size)
-                        if chunk:
-                            stderr.write(chunk)
+                while True:
+                    more_stdout, more_stderr = read(process, fd, stdout_buffer, stderr_buffer)
+                    if not more_stdout and not more_stderr:
+                        break
         if process.returncode and throw:
             raise subprocess.CalledProcessError(process.returncode, process.args, process.stdout, process.stderr)
     except Exception:
@@ -119,5 +148,5 @@ def execute(*args, text=True, env=None, throw=True, stdin=None, stdout=None, std
     if stdin_writer is not None and stdin_writer.is_alive():
         raise ChildProcessError("The child stdin writer process did not terminate successfully")
     return (process,
-            None if stdout is not None else os.linesep.join(stdout_buffer) if text else stdout_buffer,
-            None if stderr is not None else os.linesep.join(stderr_buffer) if text else stderr_buffer)
+            None if stdout is not None else os.linesep.join(stdout_buffer) if text else bytes(stdout_buffer),
+            None if stderr is not None else os.linesep.join(stderr_buffer) if text else bytes(stderr_buffer))
